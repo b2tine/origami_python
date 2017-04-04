128af9e578a4f4187cb9166511d6fc45c68a84c1
ytbwd <tengbo.yang@stonybrook.edu>
2017-04-04 14:39:14 -0400

    modify reading data

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import numpy as np
 from folding_opt import folding_opt
 import matplotlib.pyplot as plt
+import sys
 
-points = np.loadtxt("points.txt")
-creases = np.loadtxt("creases.txt", dtype=int)
-edges = np.loadtxt("edges.txt", dtype=int)
-rho_T = np.loadtxt("configs.txt")
+dirname = sys.argv[1]
+
+points = np.loadtxt(dirname + "/points.txt")
+creases = np.loadtxt(dirname + "/creases.txt", dtype=int)
+edges = np.loadtxt(dirname + "/edges.txt", dtype=int)
+rho_T = np.loadtxt(dirname + "/configs.txt")
 rho_T *= np.pi
 
 faces = []
-infile = open("faces.txt", "r")
+infile = open(dirname + "/faces.txt", "r")
 for line in infile : 
     face = map(int, line.split())
     faces.append(face)
+
+if dirname == "test3" : 
+    theta = np.linspace(0, 2*np.pi-2*np.pi/16, num=16)
+    x = np.cos(theta)
+    y = np.sin(theta)
+    z = np.zeros((16, 1))
+    x = np.reshape(x, (x.size, 1))
+    y = np.reshape(y, (y.size, 1))
+    z = np.reshape(z, (z.size, 1))
+    points = np.append(x, y, axis=1)
+    points = np.append(points, z, axis=1)
+    points *= 2
+    points = np.append(points, np.array([[0,0,0]]), axis=0)
+
+    x = np.arange(16)
+    x = np.reshape(x, (x.size, 1))
+    y = np.append(np.arange(1, 16), 1)
+    y = np.reshape(y, (y.size, 1))
+    edges = np.append(x, y, axis=1)
+
+    x = np.ones(16, dtype=int) * 16
+    x = np.reshape(x, (x.size, 1))
+    y = np.arange(16)
+    y = np.reshape(y, (y.size, 1))
+    creases = np.append(x, y, axis=1)
 
 fig = plt.figure()
 rho_series = folding_opt(fig, points, edges, creases, faces, rho_T, 500, "test")
